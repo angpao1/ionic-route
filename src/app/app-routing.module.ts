ff6fe782fec72b1441af1948f40275974cee6236
angpao <angpao.kittimasak@gmail.com>
2019-06-19 14:13:36 +0700

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' }
+  { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
+  { path: 'page1', loadChildren: './page1/page1.module#Page1PageModule' },
+  { path: 'page2', loadChildren: './page2/page2.module#Page2PageModule' },
+  { path: 'page3', loadChildren: './page3/page3.module#Page3PageModule' }
 ];
 @NgModule({
   imports: [
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -35,6 +35,33 @@ const routes: Routes = [
         ]
       },
       {
+        path: 'page1',
+        children: [
+          {
+            path: '',
+            loadChildren: '../page1/page1.module#Page1PageModule'
+          }
+        ]
+      },
+      {
+        path: 'page2',
+        children: [
+          {
+            path: '',
+            loadChildren: '../page2/page2.module#Page2PageModule'
+          }
+        ]
+      },
+      {
+        path: 'page3',
+        children: [
+          {
+            path: '',
+            loadChildren: '../page3/page3.module#Page3PageModule'
+          }
+        ]
+      },
+      {
         path: '',
         redirectTo: '/tabs/tab1',
         pathMatch: 'full'
@@ -54,4 +81,4 @@ const routes: Routes = [
   ],
   exports: [RouterModule]
 })
-export class TabsPageRoutingModule {}
+export class TabsPageRoutingModule { }
